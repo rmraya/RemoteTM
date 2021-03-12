f127370959f9150eca89dbee58ed2cb5a6f19159
Rodolfo M. Raya <rmraya@users.noreply.github.com>
2021-03-12 11:51:05 -0300

    Improved dropdown component

--- ts/dropdown.ts
+++ ts/dropdown.ts
@@ -3,34 +3,24 @@ export class DropDown {
     static DOWN: string = '<svg width="12" height="7" viewBox="0 0 12 7" xmlns="http://www.w3.org/2000/svg"><path d="M1.41 0L6 4.32659L10.59 0L12 1.33198L6 7L0 1.33198L1.41 0Z" /></svg>';
     static UP: string = '<svg width="12" height="7" viewBox="0 0 12 7" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M10.59 7L6 2.67341L1.41 7L0 5.66802L6 0L12 5.66802L10.59 7Z" fill="#FFFFFE"/></svg>';
 
-    container: HTMLDivElement;
     header: HTMLAnchorElement;
     headerText: string;
-    options: HTMLDivElement;
+    optionsArray: HTMLAnchorElement[];
     backdrop: HTMLDivElement;
     showing: boolean;
 
     constructor(parent: HTMLElement) {
-        this.container = document.createElement('div');
-        this.container.classList.add('dropdown');
+        this.optionsArray = [];
+        this.showing = false;
+
+        let container: HTMLDivElement = document.createElement('div');
+        container.classList.add('dropdown');
 
         this.header = document.createElement('a');
         this.header.addEventListener('click', () => { this.toggleOptions(); });
-        this.container.appendChild(this.header);
+        container.appendChild(this.header);
 
-        this.backdrop = document.createElement('div');
-        this.backdrop.classList.add('backdrop');
-        this.backdrop.classList.add('hidden');
-        this.backdrop.addEventListener('click', () => { this.toggleOptions(); });
-        document.body.appendChild(this.backdrop);
-
-        this.options = document.createElement('div');
-        this.options.classList.add('dropdownOptions');
-        this.options.classList.add('shadow');
-        this.backdrop.appendChild(this.options);
-
-        parent.appendChild(this.container);
-        this.showing = false;
+        parent.appendChild(container);
     }
 
     setHeaderText(header: string): void {
@@ -39,29 +29,38 @@ export class DropDown {
     }
 
     addOption(option: HTMLAnchorElement): void {
-        this.options.appendChild(option);
+        this.optionsArray.push(option);
     }
 
     toggleOptions(): void {
         this.showing = !this.showing;
         if (this.showing) {
+            this.backdrop = document.createElement('div');
+            this.backdrop.classList.add('backdrop');
+            this.backdrop.addEventListener('click', () => { this.toggleOptions(); });
+            document.body.appendChild(this.backdrop);
 
-        } else {
-            
-        }
+            let options: HTMLDivElement = document.createElement('div');
+            options.classList.add('dropdownOptions');
+            options.classList.add('shadow');
+            this.backdrop.appendChild(options);
 
-        if (this.backdrop.classList.contains('hidden')) {
-            this.header.innerHTML = this.headerText + '&nbsp;' + DropDown.UP;
-            this.backdrop.classList.remove('hidden');
+            let length: number = this.optionsArray.length;
+            for (let i = 0; i < length; i++) {
+                options.appendChild(this.optionsArray[i]);
+            }
+
             let left: number = this.header.offsetLeft;
-            let width: number = this.options.clientWidth;
+            let width: number = options.clientWidth;
             if (left + width > document.body.clientWidth) {
                 left = document.body.clientWidth - width - 4;
             }
-            this.options.style.left = left + 'px';
-            this.options.style.top = (this.header.clientTop + this.header.clientHeight) + 'px';
+            options.style.left = left + 'px';
+            options.style.top = (this.header.clientTop + this.header.clientHeight) + 'px';
+
+            this.header.innerHTML = this.headerText + '&nbsp;' + DropDown.UP;
         } else {
-            this.backdrop.classList.add('hidden');
+            document.body.removeChild(this.backdrop);
             this.header.innerHTML = this.headerText + '&nbsp;' + DropDown.DOWN;
         }
     }
