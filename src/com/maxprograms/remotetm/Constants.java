c34ad9c891e3f0bdccd5c984abdf0bb49432cfbe
Rodolfo M. Raya <rmraya@users.noreply.github.com>
2022-03-07 09:25:20 -0300

    Updated libraries

--- src/com/maxprograms/remotetm/Constants.java
+++ src/com/maxprograms/remotetm/Constants.java
@@ -18,8 +18,8 @@ public class Constants {
         // private for security
     }
 
-    public static final String VERSION = "5.1.0";
-    public static final String BUILD = "20220114_0820";
+    public static final String VERSION = "5.2.0";
+    public static final String BUILD = "20220307_0923";
 
     public static final String STATUS = "status";
     public static final String OK = "OK";
